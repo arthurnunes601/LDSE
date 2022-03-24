f9bad4ddf0bb0417cc8ffe400358cfa4fb4cbd7d
Arthur Nunes <arthurnunes02@hotmail.com>
2022-03-24 00:43:22 -0300

    Trabalho Lista Dinâmica Simplesmente Encadeada
    Trabalho de Faculdade da Cadeira de Estrutura de Dados.

---
+++ ldse.py
@@ -0,0 +1,112 @@
+class No:
+    def __init__(self, info, proximo):
+        self.info = info
+        self.prox = proximo
+
+
+"CLasse que inicia a lista dinâmica simplesmente encadeada"
+
+
+class Ldse:
+    def __init__(self):
+        self.prim = self.ult = None
+        self.quant = 0
+
+    def estahVazia(self):
+        return self.quant == 0
+
+    def inserirInicio(self, valor):
+        if self.quant == 0:
+            self.prim = self.ult = No(valor, None)
+        else:
+            self.prim = No(valor, self.prim)
+        self.quant += 1
+
+    def inserirFim(self, valor):
+        if self.quant == 0:
+            self.prim = self.ult = No(valor, None)
+        else:
+            self.ult.prox = self.ult = No(valor, None)
+        self.quant += 1
+
+    def removerInicio(self):
+        if self.quant == 1:
+            self.prim = self.ult = None
+        else:
+            self.prim = self.prim.prox
+        self.quant -= 1
+
+    def imprimir(self):
+        aux = self.prim
+        while aux != None:
+            print(aux.info, end=' ')
+            aux = aux.prox
+            print()
+
+    def getQuant(self):
+        return self.quant
+
+    def removerFim(self):
+        if self.quant == 1:
+            self.prim = self.ult = None
+        else:
+            aux = self.prim
+            while aux.prox != self.ult:
+                aux = aux.prox
+            aux.prox = None
+            self.ult = aux
+        self.quant -= 1
+
+    def getFirstElement(self):
+        if not self.estahVazia():
+            return self.prim.info
+
+    def getLastElement(self):
+        if not self.estahVazia():
+            return self.ult.info
+
+    def removerElemento(self, valor):
+        if self.quant == 1:
+            if (self.prim.info == valor):
+                self.prim = self.ult = None
+                self.quant -= 1
+        else:
+            aux = ant = self.prim
+            while aux.prox != None and aux.info != valor:
+                ant = aux
+                aux = aux.prox
+            if aux.info == valor:
+                ant.prox = aux.prox
+                if aux == self.prim:
+                    self.prim = ant.prox
+                if aux == self.ult:
+                    self.ult = ant
+                self.quant -= 1
+
+    def removerTodas(self, valor):
+        if self.quant == 1:
+            if self.prim.info == valor:
+                self.prim = self.ult = None
+                self.quant -= 1
+        else:
+            aux = ant = self.prim
+            if self.prim.info == valor:
+                self.prim = self.prim.prox
+                self.quant -= 1
+            while aux.prox != None:
+                ant = aux
+                aux = aux.prox
+                if aux.info == valor:
+                    ant.prox = aux.prox
+                    if aux == self.prim:
+                        self.prim = ant.prox
+                    if aux == self.ult:
+                        self.ult = ant
+                    self.quant -= 1
+            if self.ult.info == valor:
+                aux = self.ult
+                while aux.prox != self.ult:
+                    aux = aux.prox
+                aux.prox = None
+                self.ult = aux
+                self.quant -= 1
---
+++ main.py
@@ -0,0 +1,56 @@
+import ldse
+
+lista = ldse.Ldse()
+
+if not lista.estahVazia():
+    lista.removerInicio()
+
+lista.inserirInicio('7')
+lista.inserirInicio('7')
+lista.inserirInicio('6')
+lista.inserirInicio('5')
+lista.inserirInicio('4')
+#lista.inserirInicio('7')#
+lista.inserirInicio('3')
+lista.inserirInicio('2')
+lista.inserirInicio('1')
+lista.inserirInicio('7')
+
+print("#####################################")
+print("############### L D S E #############")
+
+lista.imprimir()
+
+print("#######################################")
+print(" # Removendo todos os elementos ... ", lista.removerTodas('7'))
+lista.imprimir()
+
+print("#####################################")
+print(" # Tamanho da lista:" , lista.getQuant())
+
+print("#####################################")
+print(" # Consultando primeiro elemento... ")
+print(" Primeiro elemento da lista: ", lista.getFirstElement())
+
+print("#######################################")
+print(" # Consultando ultimo elemento... ")
+print("Último elemento da lista: ", lista.getLastElement())
+
+print("#######################################")
+print(" # Removendo o primeiro elemento...", lista.removerInicio())
+lista.imprimir()
+
+
+print("#######################################")
+print(" # Removendo o último elemento...", lista.removerFim())
+lista.imprimir()
+
+print("#######################################")
+print(" # Removendo o elemento 4... ", lista.removerElemento('4'))
+lista.imprimir()
+
+
+
+'''print("#######################################")
+print("Somando todos os elementos... ", lista.somarElementos())
+lista.imprimir()'''
